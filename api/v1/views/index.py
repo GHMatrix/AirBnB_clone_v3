d900853f6533a0084c4bc90f7613dc5172982c97
GHMatrix <gsmensah@yahoo.com>
2023-10-01 21:42:52 +0000

    Updated index.py
    created new route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,3 +8,16 @@ from flask import jsonify
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def api_status():
     return jsonify({"status": "OK"})
+
+
+@app.route('/api/v1/stats', methods=['GET'])
+def get_stats():
+    """Retrieve the number of objects by type."""
+    classes = ["User", "Place", "City", "Amenity", "Review", "State"]
+    stats = {}
+
+    for cls_name in classes:
+        cls = getattr(storage.all(), cls_name)
+        stats[cls_name] = storage.count(cls)
+
+    return jsonify(stats)
