6199c892e727104cde8f8b8a5fe74029db7a5180
GHMatrix <gsmensah@yahoo.com>
2023-10-01 23:21:01 +0000

    updated this file
    added views import

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''views initializing file'''
 
+from api.v1.views.states import *
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
