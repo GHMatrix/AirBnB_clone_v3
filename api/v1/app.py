5119c51569a7da32a0b2c46a80e8adb0d158e39a
GHMatrix <gsmensah@yahoo.com>
2023-10-01 20:31:51 +0000

    updated file
    Added Flask instance

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,7 +8,12 @@ from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
-
+‘’’Instance of Flask web app.’’’
+app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+app_port = int(os.getenv('HBNB_API_PORT', '5000'))
+app.url_map.strict_slashes = False
+app.register_blueprint(app_views)
+CORS(app, resources={'/*': {'origins': app_host}})
 
 @app.teardown_appcontext
 def teardown(exception):
