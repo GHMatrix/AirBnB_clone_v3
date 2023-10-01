d50145c8671f518992529733865fe759455c8fd7
GHMatrix <gsmensah@yahoo.com>
2023-10-01 22:23:28 +0000

    Update app.py
    Added 404 error handling

--- api/v1/app.py
+++ api/v1/app.py
@@ -16,6 +16,12 @@ def teardown(exception):
     storage.close()
 
 
+@app.errorhandler(404)
+def error_404(error):
+    '''Handling 404 HTTP error code.'''
+    return jsonify(error='Not found'), 404
+
+
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(os.getenv('HBNB_API_PORT', 5000))
