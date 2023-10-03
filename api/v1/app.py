306774c0d7059c1551377ec166e79587b695069d
GHMatrix <gsmensah@yahoo.com>
2023-10-03 13:31:11 +0000

    Upddated app.py
    added Blueprint import

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''api app module'''
+'''api app module script'''
 
 import os
 
@@ -7,6 +7,7 @@ from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 from flask_cors import CORS
+from flask import Blueprint
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
